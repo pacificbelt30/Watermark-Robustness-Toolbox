5a3c344247d4edef20fab886b70c5c3aac9b2221
pacificbelt30 <57101176+pacificbelt30@users.noreply.github.com>
2022-11-10 17:04:54 +0900

    fix add_result.py

--- experiments/add_result.py
+++ experiments/add_result.py
@@ -29,18 +29,19 @@ def parse_args():
     return parser.parse_args()
 
 
-def file_with_suffix_exists(dirname, suffix, not_contains="", raise_error=False):
+def file_with_suffix_exists(dirname, suffix, not_contains="", raise_error=False) -> str:
     for file in os.listdir(dirname):
         if file.endswith(suffix) and (not not_contains in file or len(not_contains) == 0):
             return os.path.abspath(os.path.join(dirname, file))
     if raise_error:
         raise FileNotFoundError(f"No file found with suffix '{suffix}' in '{dirname}")
-    return False
+    # return False
+    return ''
 
 
-def run_tasks(folder, wm_config, pth_file, output_filename):
-    os.system(f"cd .. && python validate_wm.py -f {os.path.join(folder, pth_file)} -w {wm_config} "
-              f"-o {output_filename}")
+def run_tasks(folder, wm_folder, wm_config, pth_file, output_filename, atk_config):
+    os.system(f"cd .. && python validate_wm.py -f {os.path.join(wm_folder, pth_file)} -w {wm_config} "
+              f"-o {output_filename} -a {atk_config} -d {os.path.join(folder,'checkpoint.pth')}")
 
 
 def main():
@@ -48,18 +49,23 @@ def main():
 
     _, attack_name = os.path.split(args.output_dir)
     # Collect all folder paths.
-    folders, wm_configs = [], []
+    folders, wm_configs, wm_bases, atk_configs = [], [], [], []
     for folder in os.listdir(args.output_dir):
         full_path = os.path.abspath(os.path.join(args.output_dir, folder))
 
         wm_config = file_with_suffix_exists(full_path, suffix=".yaml", not_contains=attack_name)
+        atk_config = file_with_suffix_exists(full_path, suffix=".yaml", not_contains=wm_config)
+        wm_base = os.path.basename(wm_config).split('.')[0]
         if os.path.exists(os.path.join(full_path, args.filename)):
             folders.append(full_path)
             wm_configs.append(wm_config)
+            wm_bases.append(wm_base)
+            atk_configs.append(atk_config)
 
     # Create the result.json
-    for folder, wm_config in zip(tqdm(folders), wm_configs):
-        run_tasks(folder, wm_config, pth_file=args.filename, output_filename="result.json")
+    for folder, wm_config, wm_base, atk_config in zip(tqdm(folders), wm_configs, wm_bases, atk_configs):
+        wm_folder = f"./outputs/cifar10/wm/{wm_base}/00000_{wm_base}"
+        run_tasks(folder, wm_folder, wm_config, pth_file=args.filename, output_filename="result.json",atk_config=atk_config)
 
 
 if __name__ == "__main__":
