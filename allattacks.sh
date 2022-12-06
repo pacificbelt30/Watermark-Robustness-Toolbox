151180718d2f4cca1c501c4b2e46c34030a44f9a
pacificbelt30 <57101176+pacificbelt30@users.noreply.github.com>
2022-12-06 16:54:20 +0900

    merged removeViT branch

---
+++ allattacks.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# wm_dir="outputs/cifar10/wm/jia_vit/00002_jia"
+wm_dir="outputs/cifar10/wm/jia/00000_jia"
+# wm_dir="outputs/cifar10/wm/blackmarks/00000_blackmarks"
+# wm_dir="outputs/cifar10/wm/uchida/00000_uchida"
+atk_conf="configs/cifar10/attack_configs_experiments/"
+log_dir="log/"
+arr=`ls $atk_conf | grep yaml`
+#arr=`ls configs/cifar10/attack_configs | grep -v 'combined_attack[1-9]*.yaml'`
+#arr=`ls configs/cifar10/attack_configs | grep 'combined_attack[1-9]*.yaml'`
+echo $arr
+ary=(`echo $arr`)
+# echo $ary
+echo "the number of attack_config:"${#ary[@]}
+for i in `seq 1 ${#ary[@]}`
+do
+  # echo ${ary[$i-1]}
+  file=`echo ${ary[$i-1]} | sed 's/\.[^\.]*$//'`
+  # echo $file
+  echo "python3 steal.py --save --attack_config $atk_conf${ary[$i-1]} --wm_dir $wm_dir"
+  `python3 steal.py --save --attack_config $atk_conf${ary[$i-1]} --wm_dir $wm_dir > $log_dir$file".out" 2> $log_dir$file"_error.out"`
+done
--- experiments/embed_list.py
+++ experiments/embed_list.py
@@ -13,10 +13,10 @@ from multiprocessing import Process
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-w', '--wm_configs', type=str, default='../experiments/configs/cifar10_embed',
+    parser.add_argument('-w', '--wm_configs', type=str, default='./configs/cifar10/wm_configs/',
                         help="Path to config file for the watermarking scheme.")
     parser.add_argument('--no-cuda', action='store_true')
-    parser.add_argument("--pretrained_dir", default="outputs/cifar10/wm/pretrained/resnet/00000_pretrained")
+    parser.add_argument("--pretrained_dir", default="./outputs/cifar10/null_models/resnet/00000_null_model")
     parser.add_argument("--filename", type=str, default="best.pth", help="Filepath to the pretrained model.")
     parser.add_argument("-n", "--num_processes", type=int, default=1, help="Number of concurrent processes.")
     parser.add_argument("--gpu", type=str, default=None, help="Which GPU to use. Defaults to GPU with least memory.")
@@ -30,7 +30,8 @@ GPUS = [2, 3]
 def run_tasks(wm_configs, pretrained_dir, filename, index):
     for wm_config in wm_configs:
         print(f"[Process {index}] Embedding {wm_config} ..")
-        os.system(f"cd .. && python embed.py -w {wm_config} --gpu {GPUS[index]} --pretrained_dir {pretrained_dir} "
+        # os.system(f"cd .. && python embed.py -w {wm_config} --gpu {GPUS[index]} --pretrained_dir {pretrained_dir} "
+        os.system(f"python embed.py -w {wm_config} --pretrained_dir {pretrained_dir} "
                   f"--filename {filename}")
         print(f"[Process {index}] Done embedding {wm_config}!")
 
