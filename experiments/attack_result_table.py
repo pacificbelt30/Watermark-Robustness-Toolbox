f9bcc25589262805b04ef4f405c97527ee4c7eff
pacificbelt30 <57101176+pacificbelt30@users.noreply.github.com>
2023-01-12 03:20:32 +0900

    ADD plot table decision thresholds

--- experiments/attack_result_table.py
+++ experiments/attack_result_table.py
@@ -55,13 +55,25 @@ def judge(json_path):
             for loss,dt in zip(steel_losses[key][atk],decision_thresholds[key][atk]):
                 # print(arch,key,atk,loss,dt,(loss>loss_thresholds and dt > 0.0))
                 # table[key][atk].append(tmark if ((loss <= loss_thresholds) and (dt >= 0.0)) else fmark)
-                table[key][atk] += (tmark if ((loss+loss_thresholds > 0.0) and (dt < 0.0)) else fmark) + ' / '
+                if atk == 'cross_architecture_retraining':
+                    table[key][atk] += (tmark if ((dt < 0.0)) else fmark) + ' / '
+                else:
+                    table[key][atk] += (tmark if ((loss+loss_thresholds > 0.0) and (dt < 0.0)) else fmark) + ' / '
+                    # table[key][atk] += (tmark if ((dt < 0.0)) else fmark) + ' / '
             table[key][atk] = table[key][atk][:-2]
 
     print(pd.DataFrame(data=table))
     print(table.keys())
     # print(json.dumps(table,indent=2))
+    decision_thresholds = {}
+    for key in result['vit'].keys():
+        if key == 'acc':
+            continue
+        decision_thresholds[key] = {}
+        for arch in result.keys():
+            decision_thresholds[key][arch] = round(result[arch][key]['decision_thresholds']['y'][-1] / result[arch][key]['decision_thresholds']['x'][-1], 5)
     judge_plot(table, result.keys())
+    decision_thresholds_plot(decision_thresholds, result.keys())
 
 def judge_plot(table:dict, archs):
     # plot
@@ -86,10 +98,36 @@ def judge_plot(table:dict, archs):
         bbox=[0,0,1,1]
     )
     plt.savefig('test.png')
+# plt.show()
+
+def decision_thresholds_plot(table:dict, archs):
+    # plot
+    df = pd.DataFrame(data=table)
+    print(df)
+    legend = ''    
+    for key in archs:
+        legend += key + ' / '
+    legend = legend[:-2]
+    w, h = 10,5
+    plt.rcParams["font.family"] = 'HackGen35Nerd'
+    # plt.rcParams["font.size"] = 20
+    fig, ax = plt.subplots(figsize=(w,h))
+    ax.axis('off')
+    fig.subplots_adjust(left=0.26,right=0.97)
+    ax.text(0.6,-0.1,legend)
+    ax.table(
+        df.values,
+        colLabels = df.columns,
+        rowLabels = df.index,
+        rowLoc = 'right',
+        loc = 'center',
+        bbox=[0,0,1,1]
+    )
+    plt.savefig('test_dt.png')
     # plt.show()
 
 def main():
-    file = './outputs/cifar10/atk_json/result1.json'
+    file = './outputs/cifar10/atk_json/result4.json'
     judge(file)
 
 if __name__ == "__main__":
